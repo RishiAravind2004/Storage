b60d11bb67d89c19432ead9172e10a2173cfa715
Mr.Potter <95698888+RishiAravind2004@users.noreply.github.com>
2025-06-24 15:30:22 +0000

    ad

--- src/main.py
+++ src/main.py
@@ -2,19 +2,11 @@ import flet as ft
 
 
 async def main(page: ft.Page):
+    page.window_always_on_top = True
+    page.on_error = lambda e: print(f"Page Error: {e.data}")
     page.scroll = ft.ScrollMode.ADAPTIVE
     page.appbar = ft.AppBar(title=ft.Text("Geolocator Tests"))
-
-    def handle_position_change(e):
-        page.add(ft.Text(f"New position: {e.latitude} {e.longitude}"))
-
-    gl = ft.Geolocator(
-        location_settings=ft.GeolocatorSettings(
-            accuracy=ft.GeolocatorPositionAccuracy.LOW
-        ),
-        on_position_change=handle_position_change,
-        on_error=lambda e: page.add(ft.Text(f"Error: {e.data}")),
-    )
+    gl = ft.Geolocator()
     page.overlay.append(gl)
 
     settings_dlg = lambda handler: ft.AlertDialog(
@@ -24,79 +16,82 @@ async def main(page: ft.Page):
             "You are about to be redirected to the location/app settings. "
             "Please locate this app and grant it location permissions."
         ),
-        actions=[ft.TextButton(text="Take me there", on_click=handler)],
+        actions=[
+            ft.TextButton(
+                text="OK",
+                on_click=handler,
+            ),
+        ],
         actions_alignment=ft.MainAxisAlignment.CENTER,
     )
 
-    async def handle_permission_request(e):
-        p = await gl.request_permission_async(wait_timeout=60)
-        page.add(ft.Text(f"request_permission: {p}"))
+    def handle_permission_request(e):
+        page.add(ft.Text(f"request_permission: {gl.request_permission()}"))
 
-    async def handle_get_permission_status(e):
-        p = await gl.get_permission_status_async()
-        page.add(ft.Text(f"get_permission_status: {p}"))
+    def handle_has_permission(e):
+        page.add(ft.Text(f"has_permission: {gl.has_permission()}"))
 
-    async def handle_get_current_position(e):
-        p = await gl.get_current_position_async()
+    def handle_get_current_position(e):
+        p = gl.get_current_position()
         page.add(ft.Text(f"get_current_position: ({p.latitude}, {p.longitude})"))
 
-    async def handle_get_last_known_position(e):
-        p = await gl.get_last_known_position_async()
+    def handle_get_last_known_position(e):
+        p = gl.get_last_known_position()
         page.add(ft.Text(f"get_last_known_position: ({p.latitude}, {p.longitude})"))
 
-    async def handle_location_service_enabled(e):
-        p = await gl.is_location_service_enabled_async()
-        page.add(ft.Text(f"is_location_service_enabled: {p}"))
+    def handle_location_service_enabled(e):
+        page.add(
+            ft.Text(f"is_location_service_enabled: {gl.is_location_service_enabled()}")
+        )
 
-    async def handle_open_location_settings(e):
-        p = await gl.open_location_settings_async()
-        page.close(location_settings_dlg)
-        page.add(ft.Text(f"open_location_settings: {p}"))
+    def handle_open_location_settings(e):
+        page.close_dialog()
+        page.add(ft.Text(f"open_location_settings: {gl.open_location_settings()}"))
 
-    async def handle_open_app_settings(e):
-        p = await gl.open_app_settings_async()
-        page.close(app_settings_dlg)
-        page.add(ft.Text(f"open_app_settings: {p}"))
-
-    location_settings_dlg = settings_dlg(handle_open_location_settings)
-    app_settings_dlg = settings_dlg(handle_open_app_settings)
+    def handle_open_app_settings(e):
+        page.close_dialog()
+        page.add(ft.Text(f"open_app_settings: {gl.open_app_settings()}"))
 
     page.add(
         ft.Row(
-            wrap=True,
-            controls=[
+            [
                 ft.OutlinedButton(
-                    "Request Permission",
+                    "request_permission",
                     on_click=handle_permission_request,
                 ),
                 ft.OutlinedButton(
-                    "Get Permission Status",
-                    on_click=handle_get_permission_status,
+                    "has_permission",
+                    on_click=handle_has_permission,
                 ),
                 ft.OutlinedButton(
-                    "Get Current Position",
+                    "get_current_position",
                     on_click=handle_get_current_position,
                 ),
                 ft.OutlinedButton(
-                    "Get Last Known Position",
+                    "get_last_known_position",
                     visible=False if page.web else True,
                     on_click=handle_get_last_known_position,
                 ),
                 ft.OutlinedButton(
-                    "Is Location Service Enabled",
+                    "is_location_service_enabled",
                     on_click=handle_location_service_enabled,
                 ),
                 ft.OutlinedButton(
-                    "Open Location Settings",
+                    "open_location_settings",
                     visible=False if page.web else True,
-                    on_click=lambda e: page.open(location_settings_dlg),
+                    on_click=lambda e: page.show_dialog(
+                        settings_dlg(handle_open_location_settings)
+                    ),
                 ),
                 ft.OutlinedButton(
-                    "Open App Settings",
+                    "open_app_settings",
                     visible=False if page.web else True,
-                    on_click=lambda e: page.open(app_settings_dlg),
+                    on_click=lambda e: page.show_dialog(
+                        settings_dlg(handle_open_app_settings)
+                    ),
                 ),
             ],
+            wrap=True,
         )
     )
 
